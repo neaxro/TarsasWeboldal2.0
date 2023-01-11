8639ff27d8cde90b49de5a12e45cd53e07005bd0
neaxro <neaxro@gmail.com>
2023-01-11 19:08:51 +0100

    Jó a fejléc
    Eltűnik kis méret esetén
    Bootstrap példatár hozzáadva gyors kereshetőség céljából

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+    <title>Hello, world!</title>
+
+  </head>
+
+  <body>
+    <main>
+      <header class="py-3 mb-3 border-bottom">
+      <div class="container-fluid d-grid gap-3 align-items-center row" style="grid-template-columns: 1fr 2fr;">
+
+        <div class="align-items-center col-4 d-none d-md-inline-block">
+          <h1 class="weboldalcim">Társasjátékok</h1>
+        </div>
+        
+        <div class="align-items-center col-12 col-md-8 d-inline-block">
+          <form class="w-100 me-3" role="search">
+            <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+          </form>
+        </div>
+
+      </div>
+    </header>
+    </main>
+
+  </body>
+</html>
