b3c479572b4cc2fecb925b8196d1e438b4293aeb
neaxro <neaxro@gmail.com>
2023-01-11 19:49:09 +0100

    Kártyák nagyjából jók
    Túl kicsi méretben nem középre helyeződik a kártya.
    Kicsit lehet szépíteni az egyes kártyákat

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <!-- Saját stílusok CSS -->
     <link rel="stylesheet" href="style.css">
 
     <title>Hello, world!</title>
@@ -15,6 +17,7 @@
 
   <body>
     <main>
+      <!-- Oldal fejléce -->
       <header class="py-3 mb-3 border-bottom">
       <div class="container-fluid d-grid gap-3 align-items-center row" style="grid-template-columns: 1fr 2fr;">
 
@@ -31,6 +34,114 @@
       </div>
     </header>
     </main>
+  <section class="container py-3">
+  <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3">
+    
+    <div class="col">
+      <div class="card" style="width: 18rem;">
+        
+        <img src="Forrasok/pelda_kep.jpg" class="card-img-top" alt="...">
 
+        <div class="card-body">
+          <img src="Forrasok/icon_emberek.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">3-5</p>
+          <hr>
+          <img src="Forrasok/icon_ido.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">15 perc</p>
+          <hr>
+          <img src="Forrasok/icon_eletkor.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">7+</p>
+          <hr>
+          <p class="card-text">Some quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card" style="width: 18rem;">
+        
+        <img src="Forrasok/pelda_kep.jpg" class="card-img-top" alt="...">
+
+        <div class="card-body">
+          <img src="Forrasok/icon_emberek.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">3-5</p>
+          <hr>
+          <img src="Forrasok/icon_ido.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">15 perc</p>
+          <hr>
+          <img src="Forrasok/icon_eletkor.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">7+</p>
+          <hr>
+          <p class="card-text">Some quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card" style="width: 18rem;">
+        
+        <img src="Forrasok/pelda_kep.jpg" class="card-img-top" alt="...">
+
+        <div class="card-body">
+          <img src="Forrasok/icon_emberek.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">3-5</p>
+          <hr>
+          <img src="Forrasok/icon_ido.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">15 perc</p>
+          <hr>
+          <img src="Forrasok/icon_eletkor.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">7+</p>
+          <hr>
+          <p class="card-text">Some quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card" style="width: 18rem;">
+        
+        <img src="Forrasok/pelda_kep.jpg" class="card-img-top" alt="...">
+
+        <div class="card-body">
+          <img src="Forrasok/icon_emberek.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">3-5</p>
+          <hr>
+          <img src="Forrasok/icon_ido.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">15 perc</p>
+          <hr>
+          <img src="Forrasok/icon_eletkor.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">7+</p>
+          <hr>
+          <p class="card-text">Some quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card" style="width: 18rem;">
+        
+        <img src="Forrasok/pelda_kep.jpg" class="card-img-top" alt="...">
+
+        <div class="card-body">
+          <img src="Forrasok/icon_emberek.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">3-5</p>
+          <hr>
+          <img src="Forrasok/icon_ido.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">15 perc</p>
+          <hr>
+          <img src="Forrasok/icon_eletkor.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">7+</p>
+          <hr>
+          <p class="card-text">Some quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card" style="width: 18rem;">
+        
+        <img src="Forrasok/pelda_kep.jpg" class="card-img-top" alt="...">
+
+        <div class="card-body">
+          <img src="Forrasok/icon_emberek.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">3-5</p>
+          <hr>
+          <img src="Forrasok/icon_ido.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">15 perc</p>
+          <hr>
+          <img src="Forrasok/icon_eletkor.png" class="kartya-magyarazo-ikonok"> <p class="card-text kartya-magyarazo-ikonok-szoveg">7+</p>
+          <hr>
+          <p class="card-text">Some quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to buSome quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  </div>
+
+    
   </body>
 </html>
