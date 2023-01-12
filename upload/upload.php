24371a70e558ba9131eaab2cf2ef860c0a8e42bc
neaxro <neaxro@gmail.com>
2023-01-12 22:22:36 +0100

    Működik a kép hozzáadása :100:
    Már csak az sql utasítást kell kiegészíteni illetve megoldani, hogy visszaugorjon az upload.html-re

--- upload/upload.php
+++ upload/upload.php
@@ -1,8 +1,48 @@
 <?php
     if ($_SERVER['REQUEST_METHOD'] == "POST"){
-        $cim = $_POST["jatek_cim"];
-        echo $cim;
-    }
         
-    echo "<br>ASD<br>";
+        $cim = $_POST["cim"];
+        
+        $min_jatekos = $_POST["min_jatekos"];
+        $max_jatekos = $_POST["max_jatekos"];
+
+        $min_hossz = $_POST["min_hossz"];
+        $max_hossz = $_POST["max_hossz"];
+
+        $korhatar = $_POST["korhatar"];
+
+        $indexkep = $_FILES["indexkep"]["name"];
+        $temp_indexkep = $_FILES["indexkep"]["tmp_name"];
+        $kepekMappa = "../indexkepek/" . $indexkep;
+
+        // SQL kapcsolat es beszurasa
+        $servername = "192.168.1.24";
+        $username = "tarsasjatek_web";
+        $password = "Asdasd11#";
+        $dbname = "tarsasjatekok";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "INSERT INTO Tarsasok (kep) VALUES ('$indexkep');";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        // Kep mentese mappaba
+        if (move_uploaded_file($temp_indexkep, $kepekMappa)) {
+            echo "<h3>  Image uploaded successfully!</h3>";
+        } else {
+            echo "<h3>  Failed to upload image!</h3>";
+        }
+
+        $conn->close();
+    }
 ?>
