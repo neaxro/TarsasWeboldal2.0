8c441a76c6277860752063594b0d9751c337f840
neaxro <neaxro@gmail.com>
2023-01-30 17:43:39 +0100

    Hierarchia rendberakása

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     
     <!-- Saját stílusok CSS -->
-    <link rel="stylesheet" href="./main/style.css">
+    <link rel="stylesheet" href="./res/main/style.css">
 
     <!-- JQuery import -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
@@ -25,7 +25,7 @@
         <div class="d-inline-flex align-items-baseline">
           <a class="navbar-brand" href="index.php"><h2 style="color:white;">Társasjátékok</h2></a>
 
-          <a class="nav-link" href="./upload/upload.php" style="color:white;">Új hozzáadása</a>
+          <a class="nav-link" href="./res/upload/upload.php" style="color:white;">Új hozzáadása</a>
         </div>
 
         <form class="form-inline my-2">
@@ -38,9 +38,9 @@
     
     <div class="d-flex justify-content-around align-items-start flex-shrink-0 flex-grow-0 flex-wrap py-3">
       <!-- Listazas -->
-      <?php require_once("./main/listData.php") ?>
+      <?php require_once("./res/main/listData.php") ?>
     </div>
 
-    <script src="./main/script.js" type="text/javascript"></script>
+    <script src="./res/main/script.js" type="text/javascript"></script>
   </body>
 </html>
